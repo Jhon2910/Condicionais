b59df932256b667ef32bd035f31e10a7953331ed
Jonathan Rodrigues <91800109+Jhon2910@users.noreply.github.com>
2025-06-11 10:59:09 -0300

    Create jonathan9.java

---
+++ jonathan9.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class jonathan9 {
+    public static void main(String[]agrs){
+        Scanner.sc = new Scanner(System.in);
+        
+        double a = ax²+bx+c=0;
+                
+        if (a = 0){
+            System.out.print("Não é uma equação do 2º grau.");
+        }
+    }
+}
