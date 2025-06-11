7e389ae9636c57b72534cdcd51b6a4f7d8072ef6
Jonathan Rodrigues <91800109+Jhon2910@users.noreply.github.com>
2025-06-11 11:48:51 -0300

    Update jonathan2.java

--- jonathan2.java
+++ jonathan2.java
@@ -1,1 +1,20 @@
+import java.util.Scanner;
 
+public class jonathan2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite um número: ");
+        int n = sc.nextInt();
+        int resto = n % 2;
+
+
+        if (n / 2 != n % 2) {
+            System.out.print("O número que voce digitou é impar!");
+        } else {
+            System.out.print("O númeero que voce digitou é par!");
+        }
+
+        sc.close();
+    }
+}
