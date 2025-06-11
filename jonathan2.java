04a1cfb123a1c6b21b5faeeaac91007a4b4d598f
Jonathan Rodrigues <91800109+Jhon2910@users.noreply.github.com>
2025-06-11 11:55:37 -0300

    Update jonathan2.java

--- jonathan2.java
+++ jonathan2.java
@@ -8,13 +8,15 @@ public class jonathan2 {
         int n = sc.nextInt();
         int resto = n % 2;
 
-
-        if (n / 2 != n % 2) {
-            System.out.print("O número que voce digitou é impar!");
-        } else {
-            System.out.print("O númeero que voce digitou é par!");
+        if (sc.hasNextInt()){//quando tem isso não roda
+            System.out.print("Digite apenas números:");}
+        else {
+            if (resto == 0) {
+                System.out.print("O número que voce digitou é par!");
+            } else {
+                System.out.print("O númeero que voce digitou é impar!");
+            }
+            sc.close();
         }
-
-        sc.close();
     }
 }
