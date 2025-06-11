8fb271b637f001df391953bd246b4142b16a96bb
Jonathan Rodrigues <91800109+Jhon2910@users.noreply.github.com>
2025-06-11 12:16:41 -0300

    Update jonathan9.java

--- jonathan9.java
+++ jonathan9.java
@@ -1,13 +1,25 @@
 import java.util.Scanner;
 
-class jonathan9 {
-    public static void main(String[]agrs){
-        Scanner.sc = new Scanner(System.in);
-        
-        double a = ax²+bx+c=0;
-                
-        if (a = 0){
-            System.out.print("Não é uma equação do 2º grau.");
+public class jonathan9 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double a,b,c,d,r1,r2 ;
+
+        a = sc.nextDouble();
+        b = sc.nextDouble();
+        c= sc.nextDouble();
+
+        if (a == 0) {
+            System.out.print("Não é uma equação de 2° grau");
+            if (b==0){
+                System.out.print("Não é uma equação de 2° grau");
+            }
+            else {
+                r1 = -c/b;
+                System.out.print("r1");
+            }
         }
+        sc.close();
     }
 }
