5294844dbc1b643b460d003a90cb66f3b8704c22
Jonathan Rodrigues <91800109+Jhon2910@users.noreply.github.com>
2025-06-11 11:14:57 -0300

    Update jonathan1.java

--- jonathan1.java
+++ jonathan1.java
@@ -1,1 +1,31 @@
+import java.util.Scanner;
 
+public class jonathan1 {
+    public static void main(String[] args) {
+        int N;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Digite a idade do atleta:");
+        N = sc.nextInt();
+
+        if (N < 5) {
+            System.out.print("Idade invalida!");
+        }
+        else if (N >= 0 && N < 5) { //Não precisa de condicional.
+            System.out.print("Não participa!");
+        }
+        else if (N >= 5 && N <=10){
+            System.out.print("Infatil!");
+        }
+        else if (N > 10 && N < 18){
+            System.out.print("Juvenil!");
+        }
+        else if (N >= 18 && N <40){
+            System.out.print("Adulto!");
+        }
+        else {
+            System.out.print("Sênior!");
+        }
+
+        sc.close();
+    }
+}
