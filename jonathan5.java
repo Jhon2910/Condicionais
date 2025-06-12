8c5bd638879df8c1bdd4140c2c6f786c71ec42a0
Jonathan Rodrigues <91800109+Jhon2910@users.noreply.github.com>
2025-06-12 08:41:50 -0300

    Update jonathan5.java

--- jonathan5.java
+++ jonathan5.java
@@ -1,1 +1,17 @@
+import java.util.Scanner;
 
+public class jonathan5 {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digíte um caracter qualquer:");
+        String caracter = sc.nextLine() ;
+
+        String vogal = "a";
+
+        if (caracter == vogal){
+            System.out.print("É uma vogal");
+        }
+
+    }
+}
