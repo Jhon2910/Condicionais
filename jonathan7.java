1f72cb5ef3b05d5f66c4eb25db7591fcce9fc7d1
Jonathan Rodrigues <91800109+Jhon2910@users.noreply.github.com>
2025-06-13 17:41:52 -0300

    Update jonathan7.java

--- jonathan7.java
+++ jonathan7.java
@@ -7,19 +7,26 @@ public class jonathan7 {
 
         System.out.print("Digite o ano de fabricação: ");
 
-        if (sc.hasNextInt()) {
+        if (sc.hasNextInt()){
             int ano = sc.nextInt();
+            System.out.print("Digite o valor da tebela FIP do carro: ");
 
+            if(sc.hasNextDouble()){
+                double tabela =sc.nextDouble();
             if (ano >= 0 && ano < 1990) {
-                System.out.printf("O imposto a ser pago é: %.2f\n",ano * 1.1);
+                System.out.printf("O imposto a ser pago é: R$%.2f\n",tabela * 0.01);
             } else if (ano >= 1990 && ano <= 2025) {
-                System.out.printf("O imposto a ser pago é: %.2f\n",ano * 1.5);
+                System.out.printf("O imposto a ser pago é: R$%.2f\n",tabela * 0.015);
             } else {
                 System.out.println("Ano invalido!, por favor digite um ano entre 0 e 2025!");
             }
         }
+            else {
+                System.out.println("O valor invalido!");
+            }
+        }
         else {
-            System.out.println("Por favor, digite apenas numeros com 1 casa decimal!");
+            System.out.println("Por favor, digite apenas numeros inteiros!\nEx: 2022");
         }
         sc.close();
     }
