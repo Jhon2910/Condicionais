0747bfb55f56c6280cbeaec47c9438a55c5d133a
Jonathan Rodrigues <91800109+Jhon2910@users.noreply.github.com>
2025-06-13 18:08:40 -0300

    Create jonathan8(continuar).java

---
+++ jonathan8(continuar).java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class jonathan8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite um valor: ");
+        double x = sc.nextInt();
+
+        if (x <= 1 ){
+            System.out.print(1);
+        }
+
+    }
+}
