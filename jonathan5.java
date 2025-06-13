d2a09050ac91779fc2172dc1ba51ccd5d9430d69
Jonathan Rodrigues <91800109+Jhon2910@users.noreply.github.com>
2025-06-13 15:04:51 -0300

    Update jonathan5.java

--- jonathan5.java
+++ jonathan5.java
@@ -1,19 +1,21 @@
 import java.util.Scanner;
 
 public class jonathan5 {
-    public static void main(String[]args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Digíte um caracter qualquer:");
-        String caracter = sc.nextLine() ;
+            System.out.print("Digíte uma caracter qualquer:");
+            String caracter = sc.nextLine();
 
-        char letra = caracter.charAt(0) ;
-
-        if ((caracter.equals("a")) || (caracter.equals("e")) || (caracter.equals("i")) || (caracter.equals("o")) || (caracter.equals("u"))){
-            System.out.println("Vogal");
-        }
-        else {
-            System.out.print("É uma consoante");
-        }
+            if (caracter.length() != 1){
+                System.out.println("Digite apenas um caracter!");
+            }
+            else {
+                if ((caracter.equals("a")) || (caracter.equals("e")) || (caracter.equals("i")) || (caracter.equals("o")) || (caracter.equals("u"))) {
+                    System.out.println("É uma Vogal!");
+                } else {
+                    System.out.print("Não é uma Vogal!");
+                }
+            }
     }
 }
