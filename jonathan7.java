9d5655d621fdbe5bcbc581e382b239f77d1783cf
Jonathan Rodrigues <91800109+Jhon2910@users.noreply.github.com>
2025-06-13 17:42:19 -0300

    Update jonathan7.java

--- jonathan7.java
+++ jonathan7.java
@@ -9,14 +9,18 @@ public class jonathan7 {
 
         if (sc.hasNextInt()){
             int ano = sc.nextInt();
+            
             System.out.print("Digite o valor da tebela FIP do carro: ");
 
             if(sc.hasNextDouble()){
                 double tabela =sc.nextDouble();
+                
             if (ano >= 0 && ano < 1990) {
                 System.out.printf("O imposto a ser pago é: R$%.2f\n",tabela * 0.01);
+                
             } else if (ano >= 1990 && ano <= 2025) {
                 System.out.printf("O imposto a ser pago é: R$%.2f\n",tabela * 0.015);
+                
             } else {
                 System.out.println("Ano invalido!, por favor digite um ano entre 0 e 2025!");
             }
