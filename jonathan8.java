7b55b5c8ebb3bc56b8027f5c20f208e2cd8e1e89
Jonathan Rodrigues <91800109+Jhon2910@users.noreply.github.com>
2025-06-14 18:25:11 -0300

    Update and rename jonathan8(continuar).java to jonathan8.java

---
+++ jonathan8.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class jonathan8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite um valor: ");
+
+
+        if (sc.hasNextDouble()) {
+            double x = sc.nextDouble();
+
+            if (x <= 1 ) {
+                System.out.print(1);
+            }
+            else if (x > 1 && x <= 2) {
+                System.out.print(2);
+            }
+            else if (x > 2 && x <= 3) {
+                System.out.print(Math.pow(x,2));
+            }
+            else if (x > 3) {
+                System.out.print(Math.pow(x,3));
+            }
+        }
+        else  {
+            System.out.println("Digite apenas numeros reais!");
+        }
+        sc.close();
+    }
+}
