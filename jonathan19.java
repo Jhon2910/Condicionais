768dad6d7c883b0c2a5cf29acac5aedfa610065c
Jonathan Rodrigues <91800109+Jhon2910@users.noreply.github.com>
2025-06-17 07:53:23 -0300

    Update jonathan19.java

--- jonathan19.java
+++ jonathan19.java
@@ -3,17 +3,17 @@ import java.util.Scanner;
 public class jonathan19 {
     public static void main(String[]args){
         Scanner sc = new Scanner(System.in);
-        
+
         System.out.print("Digite o salário base(R$): ");
         System.out.print("Digite o tempo de serviço(anos): ");
-        
+
        if (sc.hasNextDouble()){
         Double SalarioBase = sc.nextDouble();
         Double tempo = sc.nextDouble();
-        
+
             if (SalarioBase < 200){
                 double imposto=0;
-            } 
+            }
             else if (SalarioBase >= 200 && SalarioBase <= 450) {
                 double imposto=0.03;
             }
@@ -23,23 +23,21 @@ public class jonathan19 {
             else {
                 double imposto = 0.12 ;
             }
-            
-            if (SalarioBase > 500 && tempo <= 3){
-                double gratificacao = 20 ;
+
+            if (SalarioBase > 500){
+                if (tempo<3){
+                    double gratificacao = 0.20 * SalarioBase ;
+                }
+                else {
+                    double gratificacao = 0.30 * SalarioBase ;
+                }
             }
-            else if (SalarioBase > 500 && tempo > 3){
-                double gratificacao = 30 ;
+            else if (SalarioBase < 500){
+                if (tempo<3){
+                    double gratificacao = ;
+                }
             }
-            else if (SalarioBase < 500 && tempo < 3 ){
-                double gratificacao = 23;
-            }
-            else if (SalarioBase >3 && SalarioBase <6 ){
-                double gratificacao = 35 ;
-            }
-            else if (SalarioBase > 500 ){
-                
-            }
-            
+
        }
        else {
            System.out.println("Digite apenas números!");
