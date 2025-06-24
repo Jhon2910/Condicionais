3247479f6eaba2b65ed91771861c0089547b8a81
Jonathan Rodrigues <91800109+Jhon2910@users.noreply.github.com>
2025-06-24 08:04:59 -0300

    Update jonathan10.java

--- jonathan10.java
+++ jonathan10.java
@@ -4,9 +4,36 @@ public class jonathan10 {
     public static void main(String[]args){
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Digite seu peso: ");
-        int peso = sc.nextInt();
+        System.out.print("Digite seu peso (em KG): ");
 
-        System.out.print("Digite sua altura: ");
+        if (sc.hasNextDouble()){
+            double peso = sc.nextInt();
+
+            System.out.print("Digite sua altura (em metros): ");
+            double altura = sc.nextInt();
+
+            double imc = peso/(Math.pow(altura,2)) ;
+
+            if (peso < 0 && altura < 0){
+                System.out.println("Digite um numero maior que 0!");
+            }
+            else {
+                if (imc < 18.5){
+                    System.out.printf("Seu imc é %.2f ,você está Abaixo do peso!",imc);
+                }
+                else if (imc <= 25){
+                    System.out.printf("Seu imc é %.2f ,peso normal!",imc);
+                }
+                else if (imc < 30){
+                    System.out.printf("Seu imc é %.2f ,Acima do peso!",imc);
+                }
+                else {
+                    System.out.printf("Seu imc é %.2f ,Acima do peso!",imc);
+                }
+            }
+        }
+        else {
+            System.out.println("Digite apenas numeros!");
+        }
     }
 }
