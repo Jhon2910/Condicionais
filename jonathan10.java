0d19450e9259af649afaf0c75fdb795d25650ed9
Jonathan Rodrigues <91800109+Jhon2910@users.noreply.github.com>
2025-06-24 07:37:27 -0300

    Create jonathan10.java

---
+++ jonathan10.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class jonathan10 {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite seu peso: ");
+        int peso = sc.nextInt();
+
+        System.out.print("Digite sua altura: ");
+    }
+}
