4a274ca7e85063ce5e796a3fa4c8c87390e6b664
Jonathan Rodrigues <91800109+Jhon2910@users.noreply.github.com>
2025-06-24 21:05:06 -0300

    Update jonathan10.java

--- jonathan10.java
+++ jonathan10.java
@@ -7,36 +7,36 @@ public class jonathan10 {
         System.out.print("Digite seu peso (em KG): ");
 
         if (sc.hasNextDouble()){
-            double peso = sc.nextInt();
-
+            double peso = sc.nextDouble();
             System.out.print("Digite sua altura (em metros): ");
 
             if (sc.hasNextDouble()){
+                double altura = sc.nextDouble();
 
-            double altura = sc.nextInt();
-
-            double imc = peso/(Math.pow(altura,2)) ;
-
-            if (peso < 0 && altura < 0){
-                System.out.println("Digite um numero maior que 0!");
-            }
-            else {
-                if (imc < 18.5){
-                    System.out.printf("Seu imc é %.2f ,você está Abaixo do peso!",imc);
-                }
-                else if (imc <= 25){
-                    System.out.printf("Seu imc é %.2f ,peso normal!",imc);
-                }
-                else if (imc < 30){
-                    System.out.printf("Seu imc é %.2f ,Acima do peso!",imc);
+                if (peso <= 0 || altura <= 0){
+                    System.out.println("Digite um numero maior que 0!");
                 }
                 else {
-                    System.out.printf("Seu imc é %.2f ,Acima do peso!",imc);
+                    double imc = peso/(Math.pow(altura,2)) ;
+
+                    if (imc < 18.5){
+                        System.out.printf("Seu imc é %.2f ,você está Abaixo do peso!",imc);
+                    }
+                    else if (imc < 25){
+                        System.out.printf("Seu imc é %.2f ,peso normal!",imc);
+                    }
+                    else if (imc < 30){
+                        System.out.printf("Seu imc é %.2f ,Acima do peso!",imc);
+                    }
+                    else {
+                        System.out.printf("Seu imc é %.2f ,Voce esta Obeso!",imc);
+                    }
                 }
             }
+            else {
+                System.out.println("Digite apenas numeros!");
+            }
         }
-        else {
-            System.out.println("Digite apenas numeros!");
-        }
+        sc.close();
     }
-}}
+}
