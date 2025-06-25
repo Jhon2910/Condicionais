048b992086b15567752470a838747ad3119e94d8
Jonathan Rodrigues <91800109+Jhon2910@users.noreply.github.com>
2025-06-24 22:16:53 -0300

    Create jonathan12.java

---
+++ jonathan12.java
@@ -0,0 +1,70 @@
+import java.util.Scanner;
+
+public class jonathan12{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite o ano da votação: ");
+
+        if (sc.hasNextInt()) {
+            int anoVotacao = sc.nextInt();
+
+            System.out.print("Digite o mês da votação: ");
+            if (sc.hasNextInt()) {
+                int mesVotacao = sc.nextInt();
+
+                System.out.print("Digite o dia da votação: ");
+                if (sc.hasNextInt()) {
+                    int diaVotacao = sc.nextInt();
+
+                    System.out.print("Digite o ano de nascimento: ");
+                    if (sc.hasNextInt()) {
+                        int anoNascimento = sc.nextInt();
+
+                        System.out.print("Digite o mês de nascimento: ");
+                        if (sc.hasNextInt()) {
+                            int mesNascimento = sc.nextInt();
+
+                            System.out.print("Digite o dia de nascimento: ");
+                            if (sc.hasNextInt()) {
+                                int diaNascimento = sc.nextInt();
+
+                                int idade = anoVotacao - anoNascimento;
+
+                                if (mesVotacao < mesNascimento || (mesVotacao == mesNascimento && diaVotacao < diaNascimento)) {
+                                    idade--;
+                                }
+                                
+                                System.out.printf("Idade na data da votação: %d anos.\n", idade);
+                                System.out.print("Situação de voto: ");
+
+                                if (idade < 16) {
+                                    System.out.println("Não pode votar.");
+                                } else if (idade >= 18 && idade <= 70) {
+                                    System.out.println("Voto Obrigatório.");
+                                } else {
+                                    System.out.println("Voto Facultativo.");
+                                }
+
+                            } else {
+                                System.out.println("Erro: O dia de nascimento deve ser um número inteiro.");
+                            }
+                        } else {
+                            System.out.println("Erro: O mês de nascimento deve ser um número inteiro.");
+                        }
+                    } else {
+                        System.out.println("Erro: O ano de nascimento deve ser um número inteiro.");
+                    }
+                } else {
+                    System.out.println("Erro: O dia da votação deve ser um número inteiro.");
+                }
+            } else {
+                System.out.println("Erro: O mês da votação deve ser um número inteiro.");
+            }
+        } else {
+            System.out.println("Erro: O ano da votação deve ser um número inteiro.");
+        }
+
+        sc.close();
+    }
+}
