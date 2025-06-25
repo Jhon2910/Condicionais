84f61973eefc4d3a53660514a1b50e291eb55ec3
Jonathan Rodrigues <91800109+Jhon2910@users.noreply.github.com>
2025-06-25 18:19:37 -0300

    Update jonathan11.java

--- jonathan11.java
+++ jonathan11.java
@@ -7,8 +7,8 @@ public class jonathan11 {
         System.out.print("Digite o valor do diametro: ");
         if (sc.hasNextDouble()) {
             double D = sc.nextDouble();
-            System.out.print("Digite o valor carga: ");
 
+            System.out.print("Digite o valor carga: ");
             if (sc.hasNextDouble()) {
                 double Q = sc.nextDouble();
 
@@ -16,22 +16,24 @@ public class jonathan11 {
 
                 if (D < 50) {
                     N = 6;
-                } else if (D <= 100) {
+                }
+                else if (D <= 100) {
                     N = 4;
-                } else {
+                }
+                else {
                     N = 2;
                 }
 
-                double S = ((4 * Q) / Math.PI * Math.pow(D, 2)) * N;
-                System.out.printf("O resultado e: %.2f",S);
+                double S = ((4 * Q) / (Math.PI * Math.pow(D, 2))) * N;
+                System.out.printf("O resultado é: %.2f%n", S);
 
             }
             else {
-                System.out.print("Digite apenas numeros!");
+                System.out.print("Digite apenas números!");
             }
         }
         else {
-            System.out.print("Digite apenas numeros!");
+            System.out.print("Digite apenas números!");
         }
         sc.close();
     }
